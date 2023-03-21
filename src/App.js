c685dd8901ec66b33ac3fe926ca84affafa3d284
Ali3905 <muhammadali30905@gmail.com>
2023-03-21 20:29:34 +0500

    fixes about server side rendering

--- src/App.js
+++ src/App.js
@@ -32,10 +32,10 @@ function App() {
   
   return (
     <>
-      <Router>
+      <Router basename="/mern-ecommerce-app">
     <Navbar/>
         <Routes>
-          <Route exact path='/' element={<Home />}/>
+          <Route exact path='/mern-ecommerce-app' element={<Home />}/>
           <Route exact path='/details/:id' element={<ProductDetails/>}/>
           <Route exact path='/cart' element={<Cart/>}/>
           <Route exact path='/login' element={<Login/>}/>
